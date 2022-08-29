542fa4c12b59bf485d202cd220f455d88181bc20
durga2004 <112332996+durga2004@users.noreply.github.com>
2022-08-29 13:04:59 +0530

    Add files via upload

---
+++ finding greatest number.py
@@ -0,0 +1,9 @@
+Python 3.9.13 (tags/v3.9.13:6de2ca5, May 17 2022, 16:36:42) [MSC v.1929 64 bit (AMD64)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
+>>> 
+==================================================================== RESTART: C:\Users\perur\OneDrive\python files 1.py ====================================================================
+enter the first number:4
+enter the second number: 13
+enter the third number:456
+largest number is : 4
+>>> 
