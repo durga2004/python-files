17453d5177c96de0de8cf0d9822212c45f315f00
durga2004 <112332996+durga2004@users.noreply.github.com>
2022-08-30 10:52:58 +0530

    Add files via upload

---
+++ simple iinterest 192.py
@@ -0,0 +1,7 @@
+#1
+P=float(input("enter the principal amount:"))
+N=float(input("enter the number of year :"))
+R=float(input("enter the rate of interest:"))
+#2
+SI=(P*N*R)/100
+print("simple interest: {}".format(SI))
