99d274f6837f20c86c5e672ca59ccc9eaa62485e
durga2004 <112332996+durga2004@users.noreply.github.com>
2022-09-27 20:51:38 +0530

    Add files via upload

---
+++ day2 5question.py
@@ -0,0 +1,18 @@
+Python 3.9.13 (tags/v3.9.13:6de2ca5, May 17 2022, 16:36:42) [MSC v.1929 64 bit (AMD64)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
+>>> 
+================ RESTART: C:/Users/perur/OneDrive/Desktop/67.py ================
+enter the number of terms:10
+enter the row number:5
+           1 
+          1 1 
+         1 2 1 
+        1 3 3 1 
+       1 4 6 4 1 
+      1 5 10 10 5 1 
+     1 6 15 20 15 6 1 
+    1 7 21 35 35 21 7 1 
+   1 8 28 56 70 56 28 8 1 
+  1 9 36 84 126 126 84 36 9 1 
+Sum of numbers: 16
+>>> 
