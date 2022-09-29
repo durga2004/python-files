4a4426166513180ae3e0f232b4cbf143a7af8785
durga2004 <112332996+durga2004@users.noreply.github.com>
2022-09-29 10:55:33 +0530

    Add files via upload

---
+++ day3 1question.py
@@ -0,0 +1,7 @@
+p= int(input("principal amount="))
+t= int(input("enter no.of years:"))
+a= str(input("is customer senior citizen(y/n):"))
+rate=int(input("enter rate"))
+interest=(p*t*rate)/100
+print("interest=",interest)
+    
